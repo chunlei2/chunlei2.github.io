d3bdd6eed7587906413b1c46724fe5078ae48e6d
chunlei2 <chunleiliu1995@gmail.com>
2019-07-09 11:43:23 -0500

    initial again

---
+++ restaurantReviewApp/js/indexController.js
@@ -0,0 +1,9 @@
+﻿if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+    .then(function() {
+        console.log('Registration worked!');
+    })
+    .catch(function() {
+        console.log('Registration failed!');
+    });
+}
